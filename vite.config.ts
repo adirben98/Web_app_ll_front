ebc48616ac0358c535ad31424394a601af897dc7
zxz19982 <adirben98@gmail.com>
2024-07-25 17:15:48 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +8,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+      key: fs.readFileSync("../client-key.pem"),
+      cert: fs.readFileSync("../client-cert.pem"),
     }
   }
 })
