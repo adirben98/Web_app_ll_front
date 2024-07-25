e1899dab2605baa1a133244bb49baa9f7a87dd99
zxz19982 <adirben98@gmail.com>
2024-07-25 17:26:32 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Web_app_ll_front',
+  
   plugins: [react()],
   server: {
     host:'10.10.248.166',
     https: {
       key: fs.readFileSync("../client-key.pem"),
       cert: fs.readFileSync("../client-cert.pem"),
-    }
+    },
+    port: 443
   }
 })
