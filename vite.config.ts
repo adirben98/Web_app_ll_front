0d6b024b8b63c40340c334922370bcb7ac1f304a
zxz19982 <adirben98@gmail.com>
2024-07-25 17:24:19 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   base: '/Web_app_ll_front',
   plugins: [react()],
   server: {
+    host:'10.10.248.166',
     https: {
       key: fs.readFileSync("../client-key.pem"),
       cert: fs.readFileSync("../client-cert.pem"),
